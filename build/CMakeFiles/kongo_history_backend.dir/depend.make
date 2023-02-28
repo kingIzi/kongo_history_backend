53842d875bafb51e84c677695d37cabc0bbb5ab1
kingIzi <scott.izidore@gmail.com>
2023-02-28 16:53:35 +0100

    ..

--- build/CMakeFiles/kongo_history_backend.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for kongo_history_backend.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/kongo_history_backend.dir/kongo_history_backend_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/kongo_history_backend.dir/kongo_history_backend_autogen/mocs_compilation.cpp.o: \
- /home/coco/Documents/c++/projects/PERSONAL/kongo_history_backend/build/kongo_history_backend_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
